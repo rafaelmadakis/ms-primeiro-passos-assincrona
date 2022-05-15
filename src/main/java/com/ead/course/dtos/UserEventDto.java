38414787a6298b68b4441ac4bd736abbcacf93fd
Rafael Madakis <rafaelmadakis@gmail.com>
2022-05-15 17:50:42 -0300

    primeiros passos assincronos

---
+++ src/main/java/com/ead/course/dtos/UserEventDto.java
@@ -0,0 +1,32 @@
+package com.ead.course.dtos;
+
+import com.ead.course.models.UserModel;
+import lombok.Data;
+import org.springframework.beans.BeanUtils;
+
+import java.util.UUID;
+
+/**
+ * @Author: Rafael Madakis
+ */
+@Data
+public class UserEventDto {
+
+    private UUID userId;
+    private String username;
+    private String email;
+    private String fullName;
+    private String userStatus;
+    private String userType;
+    private String phoneNumber;
+    private String cpf;
+    private String imageUrl;
+    private String actionType;
+
+    public UserModel convertToUserModel(){
+        var userModel = new UserModel();
+        BeanUtils.copyProperties(this, userModel);
+        return userModel;
+    }
+
+}
---
+++ src/main/java/com/ead/course/enums/ActionType.java
@@ -0,0 +1,11 @@
+package com.ead.course.enums;
+
+/**
+ * @Author: Rafael Madakis
+ */
+public enum ActionType {
+
+    CREATE,
+    UPDATE,
+    DELETE;
+}
